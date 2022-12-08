ad3d14296e3aeb9b02c61a44b4ada65d633dd7ac
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-12-08 17:20:00 -0300

    context and sidebar functionality

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -10,12 +10,16 @@ import {
 } from "@mui/material";
 import InboxOutlinedIcon from "@mui/icons-material/InboxOutlined";
 import MailOutlineOutlinedIcon from "@mui/icons-material/MailOutlineOutlined";
+import { UIContext } from "../../context/ui";
+import { useContext } from "react";
 
 const menuItems: string[] = ["Inbox", "Starred", "Send email", "Drafts"];
 
 export const Sidebar = () => {
+  const { sidemenuOpen, closeSideMenu } = useContext(UIContext);
+
   return (
-    <Drawer anchor="left" open={true} onClose={() => console.log("cerrando")}>
+    <Drawer anchor="left" open={sidemenuOpen} onClose={closeSideMenu}>
       <Box sx={{ width: 250 }}>
         <Box sx={{ padding: "5px 10px" }}>
           <Typography variant="h4">Menu</Typography>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import type { AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { darkTheme } from "../themes";
 import { lightTheme } from "../themes/light-theme";
+import { UIProvider } from "../context/ui";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <UIProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UIProvider>
   );
 }
